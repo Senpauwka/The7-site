a483fb12e095303028c2fa2f6c9244c1339123fb
Senpauwka <168306935+Senpauwka@users.noreply.github.com>
2026-06-13 00:58:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-3
+const text = [
+"> booting laboratory...",
+"> loading modules...",
+"> github connected",
+"> system online"
+];
+
+let line = 0;
+let char = 0;
+
+const typed = document.getElementById("typed");
+
+function type() {
+
+if(line >= text.length) return;
+
+if(char < text[line].length){
+
+typed.innerHTML += text[line][char];
+char++;
+
+setTimeout(type,50);
+
+}else{
+
+typed.innerHTML += "<br>";
+line++;
+char = 0;
+
+setTimeout(type,400);
+
+}
+
+}
+
+type();
